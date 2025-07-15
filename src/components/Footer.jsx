d2a9bdb0e8a813ec3083f5e7f7675be9430d1949
Gideon Osei Acheampong <56104340+GiddyKobby@users.noreply.github.com>
2025-07-15 20:50:59 +0000

    polished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <footer className="footer" data-aos="fade-in" data-aos-delay="200" data-aos-duration="800">
-      <p>© {new Date().getFullYear()} Gideon Osei Acheampong. All rights reserved.</p>
-      <p>
+      <p>© {year} Gideon Osei Acheampong. All rights reserved.</p>
+      <div className="footer-links">
+  
         <a href="mailto:gideonkobbyjnr@gmail.com">Email</a> |{" "}
         <a href="https://github.com/GiddyKobby" target="_blank">GitHub</a> |{" "}
         <a href="https://linkedin.com/in/gideon-osei-acheampong" target="_blank">LinkedIn</a>
@@ -12,7 +14,7 @@ const Footer = () => {
         <a href="https://x.com/giddy.kobby" target="_blank">X</a>
         <a href="https://instagram.com/giddy_kobby" target="_blank">Instagram</a>
         <a href="https://youtube.com/@giddy_kobby" target="_blank">Youtube</a>
-      </p>
+      </div>
     </footer>
   );
 };
