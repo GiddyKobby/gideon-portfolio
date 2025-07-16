b7c284862e8f27d25783e98bf1212c03f5cbd57d
Gideon Osei Acheampong <56104340+GiddyKobby@users.noreply.github.com>
2025-07-16 03:24:30 +0000

    main fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
-AOS.init();
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import './index.css';
 
-
+AOS.init();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
