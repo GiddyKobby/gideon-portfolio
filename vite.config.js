4e8578b55c7370fb5e9ed0acd813076bfa16a8c0
Gideon Osei Acheampong <56104340+GiddyKobby@users.noreply.github.com>
2025-07-18 06:25:16 +0000

    Fix base path and rebuild

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // IMPORTANT for Netlify to work with relative paths
+  base: '/', // IMPORTANT for Netlify to work with relative paths
 })
